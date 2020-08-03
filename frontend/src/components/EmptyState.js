15cf1cad53617960fdbb5e60fc76a6bd9be96236
crushoftheyear <kajsalovisaholmstrom@gmail.com>
2020-08-03 14:10:01 +0200

    Removed Lottie animation from empty state

--- frontend/src/components/EmptyState.js
+++ frontend/src/components/EmptyState.js
@@ -1,15 +1,8 @@
 import React from 'react'
-import { LottieControl } from 'components/LottieControl'
-import animation from '../img/lottie/18332-create-a-shot-list copy.json'
 
 export const EmptyState = ({ children }) => {
   return (
     <section className="empty-state">
-
-      <LottieControl
-        animation={animation}
-        width="50%"
-      />
 
       <h2>{children}</h2>
 
